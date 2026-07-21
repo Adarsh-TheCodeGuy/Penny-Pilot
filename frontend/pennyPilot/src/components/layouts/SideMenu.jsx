40e54fb0c9030d54de2d08ac2ab2ebf0ff55c78b
Adarsh-TheCodeGuy <adarshkrsingh777@gmail.com>
2026-07-21 20:13:35 +0530

    Fix Dashboard page layout

--- frontend/pennyPilot/src/components/layouts/SideMenu.jsx
+++ frontend/pennyPilot/src/components/layouts/SideMenu.jsx
@@ -51,7 +51,7 @@ const SideMenu = ({activeMenu}) => {
           className={`w-full flex items-center gap-4 text-[15px] ${
             activeMenu == items.label ? "text-white bg-primary" : ""
           } py-3 px-6 rounded-lg mb-3`}
-          onClick={() => handleClick(item.path)}
+          onClick={() => handleClick(items.path)}
         >
           <items.icon className="text-xl" />
           {items.label}
